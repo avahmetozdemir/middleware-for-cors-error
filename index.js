fca08eca0684157f612fb4374b91a23bc10e3edb
avahmetozdemir <avamertozdemir@gmail.com>
2023-02-16 12:10:03 +0300

    add middleware for cors error

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+var cors = require("cors");
+const app = express();
+app.use(cors());
+const { createProxyMiddleware } = require("http-proxy-middleware");
+app.use(
+  createProxyMiddleware({
+    target: "https://api.schiphol.nl/", //original url
+    changeOrigin: true,
+    //secure: false,
+    onProxyRes: function (proxyRes, req, res) {
+      proxyRes.headers["Access-Control-Allow-Origin"] = "*";
+    },
+  })
+);
+app.listen(5000);
